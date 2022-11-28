6c7da2232be91aab418a093eb9b0a4ebce9df384
wldusdl42 <51149514+wldusdl42@users.noreply.github.com>
2022-11-28 23:58:03 +0900

    첫번째 커밋

---
+++ src/views/Testtest.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { reactive } from 'vue'
+
+const list = reactive([])
+const onClick = () => {
+  list = [...list , 'a']
+}
+</script>
+
+<template>
+  <h1> List: {{ list.join(', ')  }} </h1>
+  <button @click='onClick'>
+    add
+  </button>
+</template>
